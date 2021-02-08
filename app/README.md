f33fb4f294362090a22cc83568176aad7449bbd4
Giancarlo Garcia Deleon <giangarcia@Giancarlos-MBP.attlocal.net>
2021-02-07 21:21:24 -0800

    First commit

---
+++ app/README.md
@@ -0,0 +1,3 @@
+# Youtube API Android 11 (API 30)
+
+This is a project that successfully runs the Youtube Player View using Android 11.
---
+++ app/src/main/java/com/example/youtubeandroid11/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.youtubeandroid11;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    // inside of any of your application's code
+    private static final String YOUTUBE_API_KEY = BuildConfig.CONSUMER_KEY;
+    public static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView =
+                (YouTubePlayerView) findViewById(R.id.player);
+        youTubePlayerView.initialize(YOUTUBE_API_KEY,
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        youTubePlayer.cueVideo("yPYZpwSpKmA");
+                        Log.i(TAG, "Video cued");
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+                    }
+                });
+    }
+}
